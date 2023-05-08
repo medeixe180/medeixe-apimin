edad8fc02be8a36c62bd8b0abb99af8b48da0f35
Jose Eduardo (Zeduardu) <zeduardu@gmail.com>
2023-05-08 10:58:30 -0300

    refactor: colocado o swagger para aparecer sempre
    Swagger aparecera sendo em producao ou desenvolvimento

--- MedeixeApi/Program.cs
+++ MedeixeApi/Program.cs
@@ -26,11 +26,13 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
